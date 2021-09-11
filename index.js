08f5bde657f85dcb664b1e471d7065e009eb1613
Jonny William <jonnywilliampr@gmail.com>
2021-09-11 09:20:41 -0300

    feat: Fazendo requisição da API do GitHub

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+
+var xhr = new XMLHttpRequest(); //instanciando uma classe
+
+xhr.open('GET', 'https://api.github.com/users/jonnywilliampr');
+
+xhr.send(null);
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4){
+        console.log(JSON.parse(xhr.responseText));
+    }
+}
+
 // JSON.parse(); //transforma o texto JSON em um objeto.
 // JSON.stringify(); //transformar um objeto em JavaScript em texto JSON.
 
-const parseJson = JSON.parse('{"id": 1,"nome": "Notebook Dell","modelo": "gamer","quantidade": 5}');
-console.log(parseJson);
+// const parseJson = JSON.parse('{"id": 1,"nome": "Notebook Dell","modelo": "gamer","quantidade": 5}');
+// console.log(parseJson);
 
-const object = {
-    Produto: "Caneta",
-    Tipo: "Esferográfica",
-    Quantidade: 1
-};
+// const object = {
+//     Produto: "Caneta",
+//     Tipo: "Esferográfica",
+//     Quantidade: 1
+// };
 
-const objectJson = JSON.stringify(object);
+// const objectJson = JSON.stringify(object);
 
-console.log(objectJson);
+// console.log(objectJson);
