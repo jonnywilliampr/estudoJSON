f52119cfe8c47733db33d909c6060563223b5a88
Jonny William <jonnywilliampr@gmail.com>
2021-09-15 22:32:53 -0300

    Fazendo requisição de API com Axios

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var minhaPromise = function(){
-    return new Promise(function(resolve, reject){
+// var minhaPromise = function(){
+//     return new Promise(function(resolve, reject){
 
-        var xhr = new XMLHttpRequest(); //instanciando uma classe
+//         var xhr = new XMLHttpRequest(); //instanciando uma classe
 
-        xhr.open('GET', 'https://api.github.com/users/jonnywilliampr');
+//         xhr.open('GET', 'https://api.github.com/users/jonnywilliampr');
 
-        xhr.send(null);
+//         xhr.send(null);
 
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
+//         xhr.onreadystatechange = function(){
+//             if(xhr.readyState === 4){
                 
-                if(xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                }else{
-                    reject("Erro");
-                }
+//                 if(xhr.status === 200){
+//                     resolve(JSON.parse(xhr.responseText));
+//                 }else{
+//                     reject("Erro");
+//                 }
 
-            }
-        }
+//             }
+//         }
 
-    });
-}
+//     });
+// }
 
-minhaPromise()
+axios.get('https://api.github.com/users/jonnywilliampr')
 .then(function(response){
     console.log(response);
 })
